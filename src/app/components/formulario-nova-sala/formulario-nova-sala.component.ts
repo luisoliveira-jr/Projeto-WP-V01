06499cef4ef8e4003c7796e0afc2f31b63ba830e
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-03-05 04:23:28 -0300

    Melhorando componente formulário com validações

--- src/app/components/formulario-nova-sala/formulario-nova-sala.component.ts
+++ src/app/components/formulario-nova-sala/formulario-nova-sala.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-nova-sala',
@@ -7,11 +8,36 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FormularioNovaSalaComponent implements OnInit {
   @Input() btnText!: string
+  salaForm!: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.salaForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      video: new FormControl('', [Validators.required])
+    })
+   }
 
+   get title() {
+    return this.salaForm.get('title')!;
+   }
+
+   get description() {
+    return this.salaForm.get('description')!;
+   }
+
+   get video() {
+    return this.salaForm.get('video')!;
+   }
+
+  submit() {
+    if (this.salaForm.invalid) {
+      return
+    }
+    console.log("Enviou formulário")
   }
 
 }
