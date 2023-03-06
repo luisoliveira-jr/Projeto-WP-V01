bb542488da12eb1ba184f77102f74a5df3c0161f
Luis Oliveira <luisoliveira.jr@hotmail.com>
2023-03-06 11:29:48 -0300

    Implementando serviço de comentarios

--- src/app/components/sala-criada/sala-criada.component.ts
+++ src/app/components/sala-criada/sala-criada.component.ts
@@ -1,18 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Moment } from 'src/app/interfaces/Moment';
-import { MomentService } from 'src/app/services/moment.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+import { Moment } from 'src/app/interfaces/Moment';
+import { Comment } from 'src/app/interfaces/Comment';
+import { MomentService } from 'src/app/services/moment.service';
+import { CommentService } from 'src/app/services/comment.service';
 import { MessagesService } from 'src/app/services/messages.service';
-
 
 @Component({
   selector: 'app-sala-criada',
   templateUrl: './sala-criada.component.html',
   styleUrls: ['./sala-criada.component.css']
 })
-export class SalaCriadaComponent implements OnInit{
+export class SalaCriadaComponent implements OnInit {
   moment?: Moment;
+  commentForm!: FormGroup
   link?: string;
 
   constructor(
@@ -20,34 +24,68 @@ export class SalaCriadaComponent implements OnInit{
     private messagesService: MessagesService,
     private route: ActivatedRoute,
     private router: Router,
-    public sanitizer: DomSanitizer) {}
+    private commentService: CommentService,
+    public sanitizer: DomSanitizer) { }
 
-    /* @Input() urlSafe!: SafeResourceUrl; */
-  
-  ngOnInit(): void { 
-    
+  /* @Input() urlSafe!: SafeResourceUrl; */
+
+  ngOnInit(): void {
+
     //id que está na URL
     const id = Number(this.route.snapshot.paramMap.get("id"))
 
     this.momentService
-    .getMoment(id)
-    .subscribe((item) => (this.moment = item.data)) ;   
+      .getMoment(id)
+      .subscribe((item) => (this.moment = item.data));
+
+    this.commentForm = new FormGroup({
+      text: new FormControl(" ", [Validators.required]),
+      username: new FormControl(" ", [Validators.required])
+    })
 
     /* this.momentService
     .getMoment(id)
     .subscribe((item) => (this.link = item.data.image)) ; */
 
-/* console.log(this.link) */
+    /* console.log(this.link) */
     /* this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(linkEmbed) */
 
   }
 
   async removeHandler(id: number) {
-    await this.momentService.removeMoment(id).subscribe();
+    this.momentService.removeMoment(id).subscribe();
 
     this.messagesService.add("Sala excluída com sucesso!");
 
     this.router.navigate(['/']);
+  }
+
+  get text() {
+    return this.commentForm.get('text')!;
+  }
+
+  get username() {
+    return this.commentForm.get('username')!;
+  }
+
+  async onSubmit(formDirective: FormGroupDirective) {
+
+    if(this.commentForm.invalid) {
+      return
+    };
+
+    const data: Comment = this.commentForm.value;
+
+    data.momentId = Number(this.moment!.id);
+
+    await this.commentService
+      .createComment(data)
+      .subscribe((comment) => this.moment!.comments!.push(comment.data));
+
+    //Resetar form
+    this.commentForm.reset();
+    formDirective.resetForm();
+
   }
 
 }
